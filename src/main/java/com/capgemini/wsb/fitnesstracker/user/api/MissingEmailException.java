70039b509031038919c36975b535ebb33879beb1
Daniel Kowalski <danielk.biznes@gmail.com>
2024-10-30 18:59:05 +0100

    Add reason when email is required

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/api/MissingEmailException.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/api/MissingEmailException.java
@@ -1,10 +1,13 @@
 package com.capgemini.wsb.fitnesstracker.user.api;
 
 import com.capgemini.wsb.fitnesstracker.exception.api.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Exception indicating that the email is missing.
  */
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Email is required")
 public class MissingEmailException extends BusinessException {
     public MissingEmailException() {
         super("Email is required. My dear boy");
