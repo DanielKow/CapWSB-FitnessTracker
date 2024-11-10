c51aba011db33f0de2f80e1a1e39980b9cf5ed72
Daniel Kowalski <danielk.biznes@gmail.com>
2024-11-10 19:13:31 +0100

    Add method to get all trainings

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingServiceImpl.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingServiceImpl.java
@@ -3,21 +3,32 @@ package com.capgemini.wsb.fitnesstracker.training.internal;
 import com.capgemini.wsb.fitnesstracker.training.api.TrainingDto;
 import com.capgemini.wsb.fitnesstracker.training.api.TrainingProvider;
 import com.capgemini.wsb.fitnesstracker.user.api.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.Optional;
 
-// TODO: Provide Impl
+@RequiredArgsConstructor
+@Slf4j
 public class TrainingServiceImpl implements TrainingProvider {
+
+    private final TrainingRepository trainingRepository;
+    private final TrainingMapper trainingMapper;
 
     @Override
     public Optional<TrainingDto> getTraining(final Long trainingId) {
         throw new UnsupportedOperationException("Not finished yet");
     }
 
+    /**
+     * Returns all trainings
+     *
+     * @return list of all trainings
+     */
     @Override
     public List<TrainingDto> getAllTrainings() {
-        return List.of();
+        return trainingRepository.findAll().stream().map(trainingMapper::toDto).toList();
     }
 
 }
