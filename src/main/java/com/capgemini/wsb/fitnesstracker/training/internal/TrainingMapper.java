acd5a6278a80171fc91380ea27c57be912663858
Daniel Kowalski <danielk.biznes@gmail.com>
2024-11-18 17:54:01 +0100

    Add mapping Training dto to entity

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingMapper.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingMapper.java
@@ -2,12 +2,23 @@ package com.capgemini.wsb.fitnesstracker.training.internal;
 
 import com.capgemini.wsb.fitnesstracker.training.api.Training;
 import com.capgemini.wsb.fitnesstracker.training.api.TrainingDto;
+import com.capgemini.wsb.fitnesstracker.user.api.User;
 import com.capgemini.wsb.fitnesstracker.user.api.UserDto;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Component;
 
+/***
+ * Maps types related to training
+ */
 @Component
 public class TrainingMapper {
-    TrainingDto toDto(Training training) {
+
+    /**
+     * Maps {@link Training} entity to {@link TrainingDto}
+     * @param training {@link Training} entity
+     * @return {@link TrainingDto}
+     */
+    TrainingDto toDto(@NotNull Training training) {
         return new TrainingDto(training.getId(),
                 new UserDto(training.getUser().getId(),
                         training.getUser().getFirstName(),
@@ -19,5 +30,24 @@ public class TrainingMapper {
                 training.getActivityType(),
                 training.getDistance(),
                 training.getAverageSpeed());
+    }
+
+    /**
+     * Maps {@link TrainingDto} to {@link Training} entity
+     * @param trainingDto {@link TrainingDto}
+     * @return {@link Training} entity
+     */
+    Training toEntity(@NotNull TrainingDto trainingDto) {
+        return new Training(trainingDto.getId(),
+                new User(trainingDto.getUser().id(),
+                        trainingDto.getUser().firstName(),
+                        trainingDto.getUser().lastName(),
+                        trainingDto.getUser().birthdate(),
+                        trainingDto.getUser().email()),
+                trainingDto.getStartTime(),
+                trainingDto.getEndTime(),
+                trainingDto.getActivityType(),
+                trainingDto.getDistance(),
+                trainingDto.getAverageSpeed());
     }
 }
